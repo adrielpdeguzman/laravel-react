c2344507ce24c745fa5a53e90c426e893b8e50b8
Adriel P. de Guzman <adrielpdeguzman@icloud.com>
2017-03-13 17:53:01 +0800

    Remove Vue dependencies, and add React dependencies and boilerplate

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -7,14 +7,10 @@
 
 require('./bootstrap');
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-Vue.component('example', require('./components/Example.vue'));
-
-const app = new Vue({
-    el: '#app'
-});
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('app')
+);
